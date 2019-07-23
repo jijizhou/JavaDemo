0b691fd3e52ee6c6d3f65b1972f23e676d5d53fa
周杰 <752552724@qq.com>
2019-07-23 14:06:28 +0800

    极光推送 接收推送消息，打开新的页面

--- app/src/main/java/com/example/javademo/receiver/MineJpushReceiver.java
+++ app/src/main/java/com/example/javademo/receiver/MineJpushReceiver.java
@@ -1,8 +1,10 @@
 package com.example.javademo.receiver;
 
-
 import cn.jpush.android.service.JPushMessageReceiver;
 
+/**
+ * 快速集成必要的类
+ */
 public class MineJpushReceiver extends JPushMessageReceiver  {
 
 }
--- app/src/main/java/com/example/javademo/receiver/MineReceiver.java
+++ app/src/main/java/com/example/javademo/receiver/MineReceiver.java
@@ -5,8 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.javademo.IndexActivity;
+
 import cn.jpush.android.api.JPushInterface;
 
+/**
+ * com.example.javademo.receiver.MineJpushReceiver  这个是快速集成必须要的类
+ * 当前类，配置相同的过滤器，也是会收到对应的消息，可以处理收到消息之后操作
+ */
 public class MineReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,12 +28,12 @@ public class MineReceiver extends BroadcastReceiver {
                 //推送通知
 
             } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
-                int a  = 1;
+
                 //当用户点击通知时的操作,打开自定义的Activity
-//                Intent i = new Intent(context, TestActivity.class);
+                Intent i = new Intent(context, IndexActivity.class);
 //                i.putExtras(bundle);
 //                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                context.startActivity(i);
+                context.startActivity(i);
             }
         } catch (Exception e) {
             e.printStackTrace();
